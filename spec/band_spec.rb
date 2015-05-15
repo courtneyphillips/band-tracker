9d3d2794a004f082f2f4ba5fb763eb998b20f0c9
Courtney <courtney.mae.phillips@gmail.com>
2015-05-15 14:06:10 -0700

    Add integration specs for validation, callbacks, etc. Tests pass.

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -1,7 +1,6 @@
 require('spec_helper')
 
 describe(Band) do
-    it { should have_many_and_belong_to_many(:venues) }
     it { should validate_presence_of(:name) }
     it { should_not allow_value("").for(:name) }
     it { should allow_value("band name").for(:name) }
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,24 +1,44 @@
 require('spec_helper')
 
-
-describe('adding a new venue', {:type => :feature}) do
-  it('will successfully add a new venue to the list of all venues') do
+describe('adding a new band', {:type => :feature}) do
+  it('will successfully add a new band to the list of all venues') do
     visit('/')
     click_link('Add New Band')
-    fill_in("name", :with => "test band")
-    page.check('')
+    fill_in("Band Name", :with => "test band")
     click_button("Add Band")
-    expect(page).to have_content("test band")
+    expect(page).to have_content("Test Band")
   end
 end
 
-describe('adding a new band', {:type => :feature}) do
-  it('will successfully add a new band to the list of all bands') do
+describe('adding a new venue and re-directing back to bands', {:type => :feature}) do
+  it('will successfully add a new venue to the database') do
+    visit('/')
+    click_link('Add New Venue')
+    fill_in("Venue Name", :with => "test venue")
+    click_button("Add Venue")
+    expect(page).to have_content("All Bands")
+  end
+end
+
+describe('listing a venue under a band', {:type => :feature}) do
+  it('will list a venue as a place a band has played') do
+    visit ('/')
+    click_link('Add New Venue')
+    fill_in("Venue Name", :with => "test venue")
+    click_button("Add Venue")
+    click_link('Add New Band')
+    fill_in("Band Name", :with => "test band")
+    page.check("venue[]")
+    click_button("Add Band")
+    click_link('Test Band')
+    expect(page).to have_content("Test Venue")
+  end
+end
+
+describe('alerting a user that there are no venues to attribute a band to', {:type => :feature}) do
+  it('will let a user know that they need to add venues to attribute a new band to them') do
     visit('/')
     click_link('Add New Band')
-    fill_in("name", :with => "test band")
-    page.check('')
-    click_button("Add Band")
-    expect(page).to have_content("test band")
+    expect(page).to have_content("no venues")
   end
 end

92314b32a5334ed9a0fd0370a2b6cc324588853b
Courtney <courtney.mae.phillips@gmail.com>
2015-05-15 09:56:05 -0700

    Add additional specs for validation.

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -3,11 +3,14 @@ require('spec_helper')
 describe(Band) do
     it { should have_many_and_belong_to_many(:venues) }
     it { should validate_presence_of(:name) }
+    it { should_not allow_value("").for(:name) }
+    it { should allow_value("band name").for(:name) }
+    it { should callback(:titlecase_name).before(:save) }
   end
 
-describe(Band) do
-  it("ensures blank band names are not submitted") do
-    new_band = Band.new({:name => ""})
-    expect(new_band.save()).to(eq(false))
-  end
-end
+
+  # describe('#titlecase_name') do
+  #   it('titlecases the name before save') do
+  #     new_band = Band.new({:name => "flaming toads"})
+  #     new_band
+  #
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -3,11 +3,6 @@ require('spec_helper')
 describe(Venue) do
   it { should have_and_belong_to_many(:bands) }
   it { should validate_presence_of(:name) }
-end
-
-describe(Venue) do
-  it("ensures blank venue names are not submitted") do
-    new_venue = Venue.new({:name => ""})
-    expect(new_venue.save()).to(eq(false))
-  end
+  it { should_not allow_value("").for(:name) }
+  it { should allow_value("venue name").for(:name) }
 end

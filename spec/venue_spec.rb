a90d5fda5b334965baad24499634fa4dbdc0bbf4
Courtney <courtney.mae.phillips@gmail.com>
2015-05-15 09:36:16 -0700

    Add specs for have_and_belong_to_many venue spec past; band spec will not respond?

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -4,8 +4,9 @@ describe(Venue) do
   it { should have_and_belong_to_many(:bands) }
 end
 
-  # 
-  # it("ensures blank venue names are not submitted") do
-  #   new_venue = Venue.new({:name => ""})
-  #   expect(new_venue.save()).to(eq(false))
-  # end
+describe(Venue) do
+  it("ensures blank venue names are not submitted") do
+    new_venue = Venue.new({:name => ""})
+    expect(new_venue.save()).to(eq(false))
+  end
+end

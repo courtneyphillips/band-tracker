bb01675915a76f8fda0507ee353d189360344cb8
Courtney <courtney.mae.phillips@gmail.com>
2015-05-15 16:14:29 -0700

    Fix naming typo causing Capybara failure.

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -3,7 +3,7 @@ require('spec_helper')
 describe('adding a new band', {:type => :feature}) do
   it('will successfully add a new band to the list of all venues') do
     visit('/')
-    click_link('Add New Band')
+    click_link('Add Band')
     fill_in("Band Name", :with => "test band")
     click_button("Add Band")
     expect(page).to have_content("Test Band")
@@ -13,7 +13,7 @@ end
 describe('adding a new venue and re-directing back to bands', {:type => :feature}) do
   it('will successfully add a new venue to the database') do
     visit('/')
-    click_link('Add New Venue')
+    click_link('Add Venue')
     fill_in("Venue Name", :with => "test venue")
     click_button("Add Venue")
     expect(page).to have_content("All Bands")
@@ -23,10 +23,10 @@ end
 describe('listing a venue under a band', {:type => :feature}) do
   it('will list a venue as a place a band has played') do
     visit ('/')
-    click_link('Add New Venue')
+    click_link('Add Venue')
     fill_in("Venue Name", :with => "test venue")
     click_button("Add Venue")
-    click_link('Add New Band')
+    click_link('Add Band')
     fill_in("Band Name", :with => "test band")
     page.check("venue[]")
     click_button("Add Band")
@@ -38,7 +38,7 @@ end
 describe('alerting a user that there are no venues to attribute a band to', {:type => :feature}) do
   it('will let a user know that they need to add venues to attribute a new band to them') do
     visit('/')
-    click_link('Add New Band')
+    click_link('Add Band')
     expect(page).to have_content("no venues")
   end
 end

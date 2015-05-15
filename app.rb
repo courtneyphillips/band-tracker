1eb137aaef36ec53185f34f17a17d434910f3cbb
Courtney <courtney.mae.phillips@gmail.com>
2015-05-15 12:09:26 -0700

    Refactor app.rb routes.

--- app.rb
+++ app.rb
@@ -8,24 +8,15 @@ get ("/") do
   erb :index
 end
 
-
-
-#delete bands, as listed on band erb; code tentative
-
 delete('/band/:id') do
   @band = Band.find(params.fetch("id").to_i())
   @band.delete()
-  @bands = Band.all
-redirect to ('/bands')
+  redirect to ('/bands')
 end
 
 get('/bands') do
   @bands = Band.all
   erb(:bands)
-end
-
-get('/venues') do
-  erb(:venues)
 end
 
 get('/add_venue') do
@@ -34,10 +25,13 @@ end
 
 post('/venues') do
   name = params.fetch("name")
-  Venue.create({:name => name})
-  erb(:venues)
+  new_venue = Venue.create({:name => name})
+  if new_venue.save()
+    redirect to('/bands')
+  else
+    erb(:errors)
+  end
 end
-
 
 get('/add_band') do
   @venues = Venue.all
@@ -48,11 +42,44 @@ end
 post('/bands') do
   @bands = Band.all
   name = params.fetch("name")
-  venues = params.fetch("venue")
+  venues = params[:venue]
   new_band = Band.create({:name => name})
+  if venues
   venues.each() do |venue|
     found_venue = Venue.find(venue)
     new_band.venues.push(found_venue)
   end
-  erb(:bands)
+  end
+  if new_band.save()
+    erb(:bands)
+  else
+  erb(:errors)
+end
+end
+
+get('/bands/:id') do
+  @band = Band.find(params.fetch('id'))
+  erb(:band)
+end
+
+get('/bands/:id/edit') do
+  @band = Band.find(params.fetch('id'))
+  @venues = Venue.all
+  erb(:edit_band)
+end
+
+patch('/bands/:id') do
+  id = params.fetch("id")
+  @band = Band.find(id)
+  if params[:venue]
+    found_venue = Venue.find(params.fetch("venue"))
+    @band.venues.push(found_venue)
+  end
+  if params[:name]
+    new_name = params.fetch("name")
+  else
+    new_name = @band.name()
+  end
+    @band.update({:name => new_name})
+    erb(:band)
 end
